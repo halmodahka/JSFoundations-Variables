e4f1068b1a2044cb717295a79215de22d7c47211
Haya <h_almodahka@icloud.com>
2021-02-01 19:24:49 +0300

    created a story

---
+++ story.js
@@ -0,0 +1,5 @@
+let name1 = "Mshary" ;
+let age = 25;
+let hobby = "Meditation";
+
+console.log("Mr./Mrs." + name1 + ", in " + age + " years, you will be " + (age + 33) + " years old. By then you'll have decades of " + hobby + " practice!");
